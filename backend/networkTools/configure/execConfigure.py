afb6409430ed976f4bc98cb888bec7985c72f4e5
codekuu <kevinwkj@outlook.com>
2020-06-19 10:12:36 +0200

    Rewrote the handler after the new api specs.

--- backend/networkTools/configure/execConfigure.py
+++ backend/networkTools/configure/execConfigure.py
@@ -22,8 +22,7 @@ class execConfigure:
         ############################
 
         # Setting standard Request Message
-        responseData = {}
-        responseData['dataFromHost'] = ''
+        responseData = []
 
         # Allowed file extensions
         ALLOWED_EXTENSIONS = set(['txt', 'conf', 'cfg'])
@@ -33,7 +32,7 @@ class execConfigure:
 
         # EXATRACTING DATA FROM REQUEST
         hostsDict = data.form.to_dict()
-        hosts = json.loads(hostsDict['hostData'])
+        hosts = json.loads(hostsDict['hosts'])
 
         # CHECK IF FILE EXIST IN REQUEST
         if 'file' not in data.files:
@@ -48,42 +47,47 @@ class execConfigure:
             for host in hosts:
 
                 with open(blacklistHosts, 'r') as blacklist:
-                    if host['host'] not in blacklist.read():
+                    try:
+                        if host['host'] not in blacklist.read():
 
-                        # CHECK THAT ALL KEYS ARE IN DATA
-                        if all(key in host for key in ('username', 'password', 'device_type', 'port', 'host')):
+                            # CHECK THAT ALL KEYS ARE IN DATA
+                            if all(key in host for key in ('username', 'password', 'device_type', 'port', 'host')):
 
-                            # CREATE AND SAVE FILE LOCALY
-                            savePath = os.getcwd() + '/networkTools/configure/CONFIGURATION_FOLDER/' + configurationFile.filename
-                            configurationFile.save(savePath, buffer_size=16384)
+                                # CREATE AND SAVE FILE LOCALY
+                                savePath = os.path.dirname(__file__) + configurationFile.filename
+                                configurationFile.save(savePath, buffer_size=16384)
 
-                            # CONNECTION DATA
-                            connectData = {
-                                'device_type': host['device_type'],
-                                'host': host['host'],
-                                'username': host['username'],
-                                'password': base64.b64decode(host['password']).decode('ascii'),
-                                'port': host['port']
-                            }
+                                # CONNECTION DATA
+                                connectData = {
+                                    'device_type': host['device_type'],
+                                    'host': host['host'],
+                                    'username': host['username'],
+                                    'password': base64.b64decode(host['password']).decode('ascii'),
+                                    'port': host['port']
+                                }
 
-                            try:
-                                # OPERATION
-                                net_connect = ConnectHandler(**connectData)
-                                dataFromHardware = net_connect.send_config_from_file(config_file=savePath)
-                                responseData['dataFromHost'] += '##### ' + host['host'] + ' #####\n\n' + dataFromHardware + '\n\n###########################\n\n'
+                                try:
+                                    # OPERATION
+                                    net_connect = ConnectHandler(**connectData)
+                                    response = net_connect.send_config_from_file(config_file=savePath)
+                                    responseData.append({'success': True, 'host': host['host'], 'software': host['device_type'], 'output': response})
 
-                            except Exception as e:
-                                info = str(e)
-                                raise Exception(info)
-                            net_connect.disconnect()
+                                except Exception as error_message:
+                                    info = str(error_message)
+                                    print(host['host'] + " - " + info)
+                                net_connect.disconnect()
+
+                            else:
+                                responseData.append({'success': False, 'host': host['host'], 'output': 'Missing data in request.'})
 
                         else:
-                            raise Exception('Missing data in request.')
-
-                    else:
-                        responseData['dataFromHost'] += '##### ' + host['host'] + ' #####\n\n' + host['host'] + ' found in blacklist, wont do anything with this host.\n\n###########################\n\n'
+                            responseData.append({'success': False, 'host': host['host'], 'output': 'Host blacklisted.'})
+                    except Exception as error_message:
+                        info = str(error_message)
+                        print(host['host'] + " - " + info)
+                        raise Exception({'success': False, 'host': host['host'], 'software': host['device_type'], 'output': 'Something went wrong with processing this host, check Webnetter console.'})
         else:
-            raise Exception('Allowed file type are txt.')
+            raise Exception('Allowed file types are ' + ALLOWED_EXTENSIONS)
 
             # Close and remove file.
             configurationFile.close()
